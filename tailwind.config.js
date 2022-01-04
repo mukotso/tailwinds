4ca559d42233960ec9c425b855cd2797a65ece0d
Mukotso <kelvinmukotso@gmail.com>
2022-01-04 12:03:48 +0300

    tailwinds now wworking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,54 +1,62 @@
 
-// tailwind.config.js
+// // tailwind.config.js
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     screens: {
+//       'tablet': '640px',
+//       // => @media (min-width: 640px) { ... }
+
+//       'laptop': '1024px',
+//       // => @media (min-width: 1024px) { ... }
+
+//       'desktop': '1280px',
+//       // => @media (min-width: 1280px) { ... }
+//     },
+
+//     colors: {
+//       'blue': '#1fb6ff',
+//       'purple': '#7e5bef',
+//       'pink': '#ff49db',
+//       'orange': '#ff7849',
+//       'green': '#13ce66',
+//       'yellow': '#ffc82c',
+//       'gray-dark': '#273444',
+//       'gray': '#8492a6',
+//       'gray-light': '#d3dce6',
+
+//       primary: {
+//         100: '#ebf8ff',
+//         300: '#90cdf4',
+//         500: '#4299e1',
+//         700: '#2b6cb0',
+//         900: '#2a4365',
+//       },
+//       secondary: {
+//         100: '#fffff0',
+//         300: '#faf089',
+//         500: '#ecc94b',
+//         700: '#b7791f',
+//         900: '#744210',
+//       },
+//     },
+//     fontFamily: {
+//       sans: ['Graphik', 'sans-serif'],
+//       serif: ['Merriweather', 'serif'],
+//     },
+
+//     extend: {
+//       boxShadow: {
+//         huge: '0 30px 60px -15px rgba(0, 0, 0, .25)'
+//       }
+//     }
+//   }
+// }
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },
-
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-
-      primary: {
-        100: '#ebf8ff',
-        300: '#90cdf4',
-        500: '#4299e1',
-        700: '#2b6cb0',
-        900: '#2a4365',
-      },
-      secondary: {
-        100: '#fffff0',
-        300: '#faf089',
-        500: '#ecc94b',
-        700: '#b7791f',
-        900: '#744210',
-      },
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-
-    extend: {
-      boxShadow: {
-        huge: '0 30px 60px -15px rgba(0, 0, 0, .25)'
-      }
-    }
-  }
+    extend: {},
+  },
+  plugins: [],
 }
